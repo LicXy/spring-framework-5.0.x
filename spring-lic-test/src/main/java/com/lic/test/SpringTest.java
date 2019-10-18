c05832ab954be5cdde97365e38412ff63a3a1940
lic <309846286@qq.com>
2019-10-18 17:13:14 +0800

    测试代码更新

--- spring-lic-test/src/main/java/com/lic/test/SpringTest.java
+++ spring-lic-test/src/main/java/com/lic/test/SpringTest.java
@@ -12,28 +12,35 @@ public class SpringTest {
 
 	public static void main(String[] args) {
 		/**
-		 * 1. 应用上下文初始化方式(一)
+		 * 1. 应用上下文初始化方式(一: Java配置类方式)
 		 */
 		AnnotationConfigApplicationContext annotationConfigApplicationContext =
 				new AnnotationConfigApplicationContext(AppConfig.class);
-		//UserService userService = annotationConfigApplicationContext.getBean(UserService.class);
-		//userService.query();
+		UserService userService = annotationConfigApplicationContext.getBean(UserService.class);
+		annotationConfigApplicationContext.getBean("");
+		userService.query();
 
 		/**
-		 * 2. 应用上下文初始化方式(二)
+		 * 1. 应用上下文初始化方式(二: 配置文件方式)
+		 */
+		AnnotationConfigApplicationContext annotationConfigApplicationContext11 =
+				new AnnotationConfigApplicationContext("springConf.xml");
+		UserService userService11 = (UserService) annotationConfigApplicationContext11.getBean("");
+		userService11.query();
+
+		/**
+		 * 2. 应用上下文初始化方式(三)
 		 */
 		ApplicationContext applicationContext1 = new ClassPathXmlApplicationContext("springConf.xml");
 		UserService userService1 = (UserService) applicationContext1.getBean("xxx");
 		userService1.query();
+
 		/**
-		 * 3(已过时). 应用上下文初始化方式(三)
+		 * 3(已过时). 应用上下文初始化方式(四)
 		 */
-		/*XmlBeanFactory bf = new XmlBeanFactory(new ClassPathResource("bean.xml"));
+		XmlBeanFactory bf = new XmlBeanFactory(new ClassPathResource("bean.xml"));
 		UserService userService2 = (UserService) bf.getBean("xxx");
-		userService2.query();*/
-
-
-
+		userService2.query();
 
 	}
 }
